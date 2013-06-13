88413436a7f8eb66a4ed6eea16533561190b02c9
Hai Lin <hai.lin.cs@gmail.com>
2013-06-13 15:11:44 -0400

    Ch2 Add document to geom

--- hai/ch2/geom.erl
+++ hai/ch2/geom.erl
@@ -1,5 +1,14 @@
+%% @author Hai Lin <hai.lin.cs@gmail.com>
+%% @doc Functions for calculating areas of geometric shapes.
+
 -module(geom).
 -export([area/2]).
+
+%% @doc Calculates the area of a rectangle, given the
+%% length and width. Returns the product
+%% of its arguments.
+
+-spec(area(number(),number()) ->number()).
 
 area(Height, Width) ->
 	Height * Width.
