42e754ce41905c0b8e02a320c107f93fc6345737
Hai Lin <hai.lin.cs@gmail.com>
2013-06-14 12:20:55 -0400

    Ch4-4 root

--- hai/ch4/powers.erl
+++ hai/ch4/powers.erl
@@ -1,7 +1,7 @@
 
 
--module(powers1).
--export([raise/2]).
+-module(powers).
+-export([raise/2, nth_root/2]).
 -include_lib("eunit/include/eunit.hrl").
 
 raise(_, Exponent) when Exponent == 0 ->
@@ -13,11 +13,11 @@ raise(Base, Exponent) when Exponent < 0 ->
 raise(Base, Exponent) when Exponent > 0 ->
 		raise(Base, Exponent, 1).
 
-raise(Base, Exponent, Acc) when Exponent == 0 ->
-	Acc;
+raise(_, Exponent, Acc) when Exponent == 0 ->
+		Acc;
 
 raise(Base, Exponent, Acc)  ->
-	raise(Base, Exponent - 1, Acc * Base).
+		raise(Base, Exponent - 1, Acc * Base).
 
 
 raise_test_() ->
@@ -27,3 +27,20 @@ raise_test_() ->
 				?_assert(raise(2, 0) =:= 1),
 				?_assert(raise(2, -3) =:= 0.125)
 		].
+
+
+nth_root(Base, Root) -> nth_root(Base, Root, Base / 2.0).
+
+nth_root(Base, Root, Approximation) ->
+		io:format("Current guess is ~p~n", [Approximation]),
+		F = raise(Approximation, Root) - Base,
+		Fprime = Approximation * raise(Approximation, Root - 1),
+		Next = Approximation - F / Fprime,
+		Diff = abs(Next - Approximation),
+		if Diff < 1.0e-8
+				-> Next;
+				true -> nth_root(Base, Root, Next)
+		end.
+
+nth_root_test_() ->
+		[?_assert(nth_root(27, 3) =:= 3.0)].
