4332b33f1eef9214cb8dbcd11e851af3af93fce3
Hai Lin <hai.lin.cs@gmail.com>
2013-06-24 23:06:13 -0400

    Clean up ask_area

--- hai/ch5/ask_area.erl
+++ hai/ch5/ask_area.erl
@@ -5,17 +5,22 @@
 
 area() ->
 	Shape = get_shape(),
-	Dimensions = get_dimensions(triangle),
+	io:format("~p~n", [Shape]),
 	if 
-		Dimensions /= ok ->
-			geom:area({Shape, element(1, Dimensions), element(2, Dimensions)});
-		true -> ok
+		Shape =:= unknown ->
+			{error, ["Unknown Shpae"]};
+		true -> 
+			get_area(Shape)
 	end.
+
+get_area(Shape) -> 
+	Dimensions = get_dimensions(Shape),
+	geom:area({Shape, element(1, Dimensions), element(2, Dimensions)}).
 
 get_shape()  ->
 	Char = io:get_chars("R)ectangle, T)riangle, or E)llipse > ", 1),
-	io:get_line(Shape), %% Without this line, the enter will buffer to next io:get_line.
-	char_to_shape(Char).
+	io:get_line(Char), %% Without this line, the enter will buffer to next io:get_line.
+	char_to_shape(hd(Char)).
 
 get_number(Prompt) ->
 	P =	"Enter " ++ Prompt ++ " > ",
@@ -29,32 +34,32 @@ string_to_number(String) ->
 		_ -> element(1, Number)
 	end.
 
-get_dimensions(Shape) when Shape == triangle ->
+get_dimensions(Shape) when Shape =:= triangle ->
 	Dimension1 = get_number("width"),
 	Dimension2 = get_number("height"),
 	{Dimension1, Dimension2}; 
 
-get_dimensions(Shape) when Shape == rectangle ->
+get_dimensions(Shape) when Shape =:= rectangle ->
 	Dimension1 = get_number("base"),
 	Dimension2 = get_number("height"),
 	{Dimension1, Dimension2}; 
 
-get_dimensions(Shape) when Shape == ellipse ->
+get_dimensions(Shape) when Shape =:= ellipse ->
 	Dimension1 = get_number("axis"),
 	Dimension2 = get_number("axis"),
 	{Dimension1, Dimension2}; 
 
-get_dimensions(Shape) when Shape == unknown ->
+get_dimensions(Shape) when Shape =:= unknown ->
 	io:format("Unknown shape").		
 
 char_to_shape(Char)  ->
 	case Char of
-		"T" -> triangle;
-		"t" -> triangle;
-		"R" -> rectangle;
-		"r" -> rectangle;
-		"E" -> ellipse;
-		"e" -> ellipse;
+		$T -> triangle;
+		$t -> triangle;
+		$R -> rectangle;
+		$r -> rectangle;
+		$E -> ellipse;
+		$e -> ellipse;
 		_ -> unknown
 	end.
 
@@ -67,10 +72,11 @@ string_to_number_test_() ->
 	].
 
 char_to_shape_test_() ->
-	[?_assert(char_to_shape("T") =:= triangle),
-		?_assert(char_to_shape("t") =:= triangle),
-		?_assert(char_to_shape("R") =:= rectangle),
-		?_assert(char_to_shape("r") =:= rectangle),
-		?_assert(char_to_shape("E") =:= ellipse),
-		?_assert(char_to_shape("e") =:= ellipse)
+	[?_assert(char_to_shape($T) =:= triangle),
+		?_assert(char_to_shape($t) =:= triangle),
+		?_assert(char_to_shape($x) =:= unknown),
+		?_assert(char_to_shape($R) =:= rectangle),
+		?_assert(char_to_shape($r) =:= rectangle),
+		?_assert(char_to_shape($E) =:= ellipse),
+		?_assert(char_to_shape($e) =:= ellipse)
 	].
