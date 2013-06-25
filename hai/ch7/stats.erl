3e0efc6f0be8cd8476c039b15789731aefde2376
Hai Lin <hai.lin.cs@gmail.com>
2013-06-25 12:49:20 -0400

    Add stdv

--- hai/ch7/stats.erl
+++ hai/ch7/stats.erl
@@ -1,6 +1,6 @@
 
 -module(stats).
--export([minimum/1, range/1, maximum/1, mean/1]).
+-export([minimum/1, range/1, maximum/1, mean/1, stdv/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 range(Numbers) -> [ minimum(Numbers), maximum(Numbers) ].
@@ -26,10 +26,20 @@ minimum([H | T], Minimum) ->
 	end.
 
 mean(NumberList) ->
-	Sum = fun(Value, Accumulator) -> Value + Accumulator end,
-	SumNumber = lists:foldl(Sum, 0, NumberList),
-	SumNumber / length(NumberList).
+	Sum = lists:foldl(fun sum/2, 0, NumberList),
+	Sum / length(NumberList).
 
+sum(Value, Accumulator) ->
+	Value + Accumulator.
+
+sum_and_square_sum(NumberList) ->
+	SumAndSquareSum = fun(Value, [H , T]) -> [Value + H, Value * Value + T] end,
+	lists:foldl(SumAndSquareSum, [0, 0], NumberList).
+
+stdv(NumberList) ->
+	[Sum, SquareSum] = sum_and_square_sum(NumberList),
+	Length = length(NumberList),
+	math:sqrt((Length * SquareSum - Sum * Sum) / (Length * (Length - 1))).
 
 
 minimum_test_() ->
@@ -52,3 +62,19 @@ mean_test_() ->
 		?_assert(mean([1, 0, 3]) =:= 1.3333333333333333),
 		?_assert(mean([3, 3, 3]) =:= 3.0)
 	].
+
+sum_test_() ->
+	[?_assert(sum(0,1) =:= 1),
+		?_assert(sum(2,1) =:= 3),
+		?_assert(sum(-1,1) =:= 0),
+		?_assert(sum(-1.5,1) =:= -0.5)
+	].
+
+sum_and_square_sum_test_() ->
+	[?_assert(sum_and_square_sum([1, 2, 3]) =:= [6, 14]),
+		?_assert(sum_and_square_sum([-1, 2, 3]) =:= [4, 14]),
+		?_assert(sum_and_square_sum([0, 0, 0]) =:= [0, 0])
+	].
+
+stdv_test_() ->
+	[?_assert(stdv([7, 2, 9]) =:= 3.605551275463989)].
