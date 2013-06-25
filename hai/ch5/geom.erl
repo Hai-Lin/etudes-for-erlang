d3bbb25a4922120ac05306b9331896794091e4e6
Hai Lin <hai.lin.cs@gmail.com>
2013-06-24 22:33:39 -0400

    Clean up Ch5

--- hai/ch5/geom.erl
+++ hai/ch5/geom.erl
@@ -2,7 +2,7 @@
 %% @doc Functions for calculating areas of geometric shapes.
 
 -module(geom).
--export([ask_area/0, get_number/1, get_shape/1]).
+-export([area/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 %% @doc Calculates the area of a shape, given the name of the shape
@@ -20,76 +20,8 @@ area({Shape, Dim1, Dim2}) when Dim1 >= 0, Dim2 >=0 ->
 			Dim1 * Dim2;
 		true -> 0
 	end.
-
-
-ask_area() ->
-	Shape = get_shape(),
-	Dimensions = get_dimensions(Shape),
-	if 
-		Dimensions /= ok ->
-			area({Shape, element(1, Dimensions), element(2, Dimensions)});
-		true -> ok
-	end.
-
-get_shape()  ->
-	Char = io:get_chars("R)ectangle, T)riangle, or E)llipse >", 1),
-	char_to_shape(Char).
-
-get_number(Prompt) ->
-	P =	"Enter " ++ Prompt ++ " > ",
-	Result = io:get_line(P),
-	io:format(Result),
-	string_to_number(Result).
-
-string_to_number(String) ->
-	Number = string:to_float(String),
-	case Number of 
-		{error,_} -> element(1, string:to_integer(String));
-		_ -> element(1, Number)
-	end.
-
-get_dimensions(Shape) when Shape == triangle ->
-	Dimension1 = get_number("width"),
-	Dimension2 = get_number("height"),
-	{Dimension1, Dimension2}; 
-
-get_dimensions(Shape) when Shape == rectangle ->
-	Dimension1 = get_number("base"),
-	Dimension2 = get_number("height"),
-	{Dimension1, Dimension2}; 
-
-get_dimensions(Shape) when Shape == ellipse ->
-	Dimension1 = get_number("axis"),
-	Dimension2 = get_number("axis"),
-	{Dimension1, Dimension2}; 
-
-get_dimensions(Shape) when Shape == unknown ->
-	io:format("Unknown shape").		
-
-char_to_shape(Char)  ->
-	case Char of
-		"T" -> triangle;
-		"t" -> triangle;
-		"R" -> rectangle;
-		"r" -> rectangle;
-		"E" -> ellipse;
-		"e" -> ellipse;
-		_ -> unknown
-	end.
-
-
-string_to_number_test_() ->
-	[?_assert(string_to_number("5.5") =:= 5.5),
-		?_assert(string_to_number("5") =:= 5),
-		?_assert(string_to_number("0") =:= 0),
-		?_assert(string_to_number("-1") =:= -1)
-	].
-
-char_to_shape_test_() ->
-	[?_assert(char_to_shape("T") =:= triangle),
-		?_assert(char_to_shape("t") =:= triangle),
-		?_assert(char_to_shape("R") =:= rectangle),
-		?_assert(char_to_shape("r") =:= rectangle),
-		?_assert(char_to_shape("E") =:= ellipse),
-		?_assert(char_to_shape("e") =:= ellipse)
+area_test_() ->
+	[?_assert(area({ellipse, 7, 3}) =:= 65.97344572538566), 
+		?_assert(area({triangle, 7, 3}) =:= 10.5), 
+		?_assert(area({rectangle, 7, 3}) =:= 21) 
 	].
