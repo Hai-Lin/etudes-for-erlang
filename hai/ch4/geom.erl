94eb8b7728697289c1bb5e6272d1787d62a61d7a
Hai Lin <hai.lin.cs@gmail.com>
2013-06-27 14:10:43 -0400

    Format Ch4

--- hai/ch4/geom.erl
+++ hai/ch4/geom.erl
@@ -11,17 +11,17 @@
 -spec(area({atom(),number(),number()}) ->number()).
 
 area({Shape, Dim1, Dim2}) when Dim1 >= 0, Dim2 >=0 ->
-	case Shape of 
-		ellipse -> 
-			math:pi() * Dim1 * Dim2; 
-		triangle ->
-			(Dim1 * Dim2) / 2;
-		rectangle ->
-			Dim1 * Dim2;
-		true -> 0
-	end.
+  case Shape of 
+    ellipse -> 
+      math:pi() * Dim1 * Dim2; 
+    triangle ->
+      (Dim1 * Dim2) / 2;
+    rectangle ->
+      Dim1 * Dim2;
+    true -> 0
+  end.
 area_test_() ->
-	[?_assert(area({ellipse, 7, 3}) =:= 65.97344572538566), 
-		?_assert(area({triangle, 7, 3}) =:= 10.5), 
-		?_assert(area({rectangle, 7, 3}) =:= 21) 
-	].
+  [?_assert(area({ellipse, 7, 3}) =:= 65.97344572538566), 
+   ?_assert(area({triangle, 7, 3}) =:= 10.5), 
+   ?_assert(area({rectangle, 7, 3}) =:= 21) 
+  ].
