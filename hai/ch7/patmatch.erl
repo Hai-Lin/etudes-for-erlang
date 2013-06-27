c02c57160476a1df59454b083ed63729f60c0761
Hai Lin <hai.lin.cs@gmail.com>
2013-06-27 14:01:49 -0400

    Format Ch7

--- hai/ch7/patmatch.erl
+++ hai/ch7/patmatch.erl
@@ -5,19 +5,19 @@
 
 
 older_males(People) ->
-	[Name || {Name, Gender, Age} <- People, Gender == $M, Age > 40].
+  [Name || {Name, Gender, Age} <- People, Gender == $M, Age > 40].
 
 older_people(People) ->
-	[Name || {Name, Gender, Age} <- People, Gender == $M orelse Age > 40].
+  [Name || {Name, Gender, Age} <- People, Gender == $M orelse Age > 40].
 
 older_males_test_() ->
-	People = [{"Federico", $M, 22}, {"Kim", $F, 45}, {"Hansa", $F, 30},
-		{"Tran", $M, 47}, {"Cathy", $F, 32}, {"Elias", $M, 50}],
-	[?_assert(older_males(People) =:= ["Tran", "Elias"])
-	].
+  People = [{"Federico", $M, 22}, {"Kim", $F, 45}, {"Hansa", $F, 30},
+            {"Tran", $M, 47}, {"Cathy", $F, 32}, {"Elias", $M, 50}],
+  [?_assert(older_males(People) =:= ["Tran", "Elias"])
+  ].
 
 older_people_test_() ->
-	People = [{"Federico", $M, 22}, {"Kim", $F, 45}, {"Hansa", $F, 30},
-		{"Tran", $M, 47}, {"Cathy", $F, 32}, {"Elias", $M, 50}],
-	[?_assert(older_people(People) =:= ["Federico", "Kim", "Tran", "Elias"])
-	].
+  People = [{"Federico", $M, 22}, {"Kim", $F, 45}, {"Hansa", $F, 30},
+            {"Tran", $M, 47}, {"Cathy", $F, 32}, {"Elias", $M, 50}],
+  [?_assert(older_people(People) =:= ["Federico", "Kim", "Tran", "Elias"])
+  ].
