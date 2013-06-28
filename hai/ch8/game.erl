1ddeb7fae40ab7b3b0ea51114459e4ac287dd247
Hai Lin <hai.lin.cs@gmail.com>
2013-06-28 10:27:56 -0400

    Add player to game

--- hai/ch8/game.erl
+++ hai/ch8/game.erl
@@ -4,10 +4,16 @@
 
 
 player(Cards) ->
-    receive
-      {From, NumbersOfCards} ->
-        {GiveAwayCards, LeftCards} = lists:split(NumbersOfCards, Cards),
-        From ! {GiveAwayCards},
-        io:format("Play start with cards: ~p~n", [LeftCards]),
-        player(LeftCards)
-    end.
+  receive
+    {From, NumbersOfCards} ->
+      if length(Cards) >= NumbersOfCards ->
+          {GiveAwayCards, LeftCards} = lists:split(NumbersOfCards, Cards),
+          From ! {ok, GiveAwayCards},
+          io:format("Play start with cards: ~p~n", [LeftCards]),
+          player(LeftCards);
+        true ->
+          io:format("I lose ~n"),
+          From ! {"I lose"}
+      end
+  end.
+
