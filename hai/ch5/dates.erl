0fbfaca688d7e6d2f034849f82b255fc18df21fa
Hai Lin <hai.lin.cs@gmail.com>
2013-07-01 16:49:07 -0400

    Reformat dates

--- hai/ch5/dates.erl
+++ hai/ch5/dates.erl
@@ -5,12 +5,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 data_parts(Date) ->
-	[Year, Month, Day] = re:split(Date, "-", [{return, list}]),
-	[element(1,string:to_integer(Year)),
-		element(1,string:to_integer(Month)),
-		element(1,string:to_integer(Day))].
+  [Year, Month, Day] = re:split(Date, "-", [{return, list}]),
+  [element(1,string:to_integer(Year)),
+   element(1,string:to_integer(Month)),
+   element(1,string:to_integer(Day))].
 
 data_parts_test_() ->
-	[?_assert(data_parts("1982-06-29") =:= [1982,06,29]),
-		?_assert(data_parts("1998-07-29") =:= [1998,07,29])
-	].
+  [?_assert(data_parts("1982-06-29") =:= [1982,06,29]),
+   ?_assert(data_parts("1998-07-29") =:= [1998,07,29])
+  ].
--- hai/ch8/game.erl
+++ hai/ch8/game.erl
@@ -1,8 +1,7 @@
-
 -module(game).
 -export([player/1, dealer/1]).
 
-dealer({init, _}) ->
+dealer({init, _, _}) ->
   io:format("Dealers init game~n"),
   io:format("Init player1~n"),
   Player1 = spawn(game, player, [[1,2,3,4]]),
@@ -10,7 +9,7 @@ dealer({init, _}) ->
   Player2 = spawn(game, player, [[5,4,3,2]]),
   dealer({pre_battle, [Player1, Player2]});
 
-dealer({pre_battle, Players}) ->
+dealer({pre_battle, Players, _}) ->
   io:format("PreBattle start with players: ~p~n",[Players]),
   [Player1, Player2] = Players,
   io:format("Ask player1 ~p for cards~n", [Player1]),
@@ -18,6 +17,17 @@ dealer({pre_battle, Players}) ->
   io:format("Ask player2 ~p for cards~n", [Player2]),
   Player2 ! {self(), 1},
   dealer({await_battle, [Player1, Player2]});
+
+dealer({await_battle, Players, Piles, Piles1, Piles2, NumberOfResponds }) ->
+  io:format("Await Battle~n"),
+  [Player1, Player2] = Players
+  receive
+    {ok, PlayerPid, Cards} ->
+      NewPiles = lists:append(Piles, Cards),
+      case PlayerPid of
+        Player1 ->
+          
+  end;
 
 dealer(_) ->
   io:format("Ending...~n").
@@ -27,7 +37,7 @@ player(Cards) ->
     {From, NumbersOfCards} ->
       if length(Cards) >= NumbersOfCards ->
           {GiveAwayCards, LeftCards} = lists:split(NumbersOfCards, Cards),
-          From ! {ok, GiveAwayCards},
+          From ! {ok, self(), GiveAwayCards},
           io:format("Player ~p start with cards: ~p~n", [self(), LeftCards]),
           player(LeftCards);
         true ->
