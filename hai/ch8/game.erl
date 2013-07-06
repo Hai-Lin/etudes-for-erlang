789210ed2c86b6df1c546b1b154f7faa5fa9f89f
Hai Lin <hai.lin.cs@gmail.com>
2013-07-05 20:59:50 -0400

    Add war to game

--- hai/ch8/game.erl
+++ hai/ch8/game.erl
@@ -7,21 +7,27 @@ dealer({init, _, _, _, _, _}) ->
   io:format("Init player1~n"),
   Player1 = spawn(game, player, [[1,2,0,1]]),
   io:format("Init player2~n"),
-  Player2 = spawn(game, player, [[5,4,3,2]]),
+  Player2 = spawn(game, player, [[1,4,3,2]]),
   dealer({pre_battle, [Player1, Player2],[], [], [], 0});
 
-dealer({pre_battle, Players, _, _, _, _}) ->
-  io:format("PreBattle start with players: ~p~n",[Players]),
-  [Player1, Player2] = Players,
+dealer({pre_battle, [Player1, Player2], Piles, Piles1, Piles2, NumberOfResponds}) when NumberOfResponds =< 1->
+  io:format("PreBattle start with players: ~p~n",[[Player1, Player2]]),
   io:format("Ask player1 ~p for cards~n", [Player1]),
   Player1 ! {self(),ask_for_cards, 1},
   io:format("Ask player2 ~p for cards~n", [Player2]),
   Player2 ! {self(),ask_for_cards, 1},
-  dealer({await_battle, [Player1, Player2],[], [], [], 0});
+  dealer({await_battle, [Player1, Player2], Piles, Piles1, Piles2, NumberOfResponds});
 
-dealer({await_battle, Players, Piles, Piles1, Piles2, NumberOfResponds}) ->
+dealer({pre_battle, [Player1, Player2], Piles, Piles1, Piles2, NumberOfResponds}) ->
+  io:format("War start with players: ~p~n",[[Player1, Player2]]),
+  io:format("Ask player1 ~p for cards~n", [Player1]),
+  Player1 ! {self(),ask_for_cards, 3},
+  io:format("Ask player2 ~p for cards~n", [Player2]),
+  Player2 ! {self(),ask_for_cards, 3},
+  dealer({await_battle, [Player1, Player2], Piles, Piles1, Piles2, NumberOfResponds});
+
+dealer({await_battle, [Player1, Player2], Piles, Piles1, Piles2, NumberOfResponds}) ->
   io:format("Await Battle~n"),
-  [Player1, Player2] = Players,
   receive
     {ok, PlayerPid, Cards} ->
       NewPiles = lists:append(Piles, Cards),
@@ -31,29 +37,29 @@ dealer({await_battle, Players, Piles, Piles1, Piles2, NumberOfResponds}) ->
           case NumberOfResponds of
             0 ->
               io:format("Call dealer await_battle for player2~n"),
-              dealer({await_battle, Players, NewPiles, Cards, Piles2, NumberOfResponds + 1});
+              dealer({await_battle, [Player1, Player2], NewPiles, Cards, Piles2, NumberOfResponds + 1});
             1 ->
               io:format("Call dealer check_cards~n"),
-              dealer({check_cards, Players, NewPiles, Cards, Piles2, NumberOfResponds + 1})
+              dealer({check_cards, [Player1, Player2], NewPiles, Cards, Piles2, NumberOfResponds + 1})
           end;
         Player2 ->
           io:format("Receive cards from player2~n"),
           case NumberOfResponds of
             0 ->
               io:format("Call dealer await_battle for player1~n"),
-              dealer({await_battle, Players, NewPiles, Piles1, Cards, NumberOfResponds + 1});
+              dealer({await_battle, [Player1, Player2], NewPiles, Piles1, Cards, NumberOfResponds + 1});
             1 ->
               io:format("Call dealer check_cards~n"),
-              dealer({check_cards, Players, NewPiles, Piles1, Cards, NumberOfResponds + 1})
+              dealer({check_cards, [Player1, Player2], NewPiles, Piles1, Cards, NumberOfResponds + 1})
           end
       end;
     {lose, PlayerPid} ->
-      io:format("Player ~p is lose ~n", [PlayerPid])
+      io:format("Player ~p is lose, game over ~n", [PlayerPid])
   end;
 
 dealer({check_cards, [Player1, Player2], Piles, Piles1, Piles2, _}) ->
   CompareResult = compare(Piles1, Piles2),
-  case CompareResult of 
+  case CompareResult of
     bigger ->
       Player1 ! {give_cards, Piles},
       dealer({pre_battle, [Player1,Player2], [], [], [], 0});
