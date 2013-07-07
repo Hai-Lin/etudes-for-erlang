3b11d3820e275f652cda564cb02be6db95b1679a
Hai Lin <hai.lin.cs@gmail.com>
2013-07-07 18:29:55 -0400

    Move compare to cards module

--- hai/ch8/cards.erl
+++ hai/ch8/cards.erl
@@ -1,6 +1,7 @@
 
 -module(cards).
--export([make_deck/0, show_deck/1]).
+-export([make_deck/0, show_deck/1, compare/2]).
+-include_lib("eunit/include/eunit.hrl").
 
 make_deck() ->
   Deck =  [{Value, Suit} || Value <- ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
@@ -15,3 +16,37 @@ shuffle([], Acc) -> Acc;
 shuffle(List, Acc) ->
   {Leading, [H | T]} = lists:split(random:uniform(length(List)) - 1, List),
   shuffle(Leading ++ T, [H | Acc]).
+
+compare(Piles1, Piles2) ->
+  io:format("Now compare ~p with ~p ~n", [Piles1, Piles2]),
+  LastOfPiles1 = lists:last(Piles1),
+  LastOfPiles2 = lists:last(Piles2),
+  {Value1, _} = LastOfPiles1,
+  {Value2, _} = LastOfPiles2,
+  if Value1 == Value2 ->
+      io:format("~p equals to ~p ~n", [Piles1, Piles2]),
+      equal;
+    Value1 =:= "A" ->
+      io:format("~p is bigger than  ~p ~n", [Piles1, Piles2]),
+      bigger;
+    Value2 =:= "A" ->
+      io:format("~p is smaller than  ~p ~n", [Piles1, Piles2]),
+      smaller;
+    Value1 > Value2 ->
+      io:format("~p is bigger than  ~p ~n", [Piles1, Piles2]),
+      bigger;
+    Value1 < Value2 ->
+      io:format("~p is smaller than  ~p ~n", [Piles1, Piles2]),
+      smaller
+  end.
+
+compare_test_() ->
+  [?_assert(compare([{7,"Clubs"},{9,"Diamonds"}],
+                    [{10,"Diamonds"},{10,"Clubs"}]) =:= smaller),
+   ?_assert(compare([{7,"Clubs"},{"Q","Diamonds"}],
+                    [{10,"Diamonds"},{10,"Clubs"}]) =:= bigger),
+   ?_assert(compare([{7,"Clubs"},{"Q","Diamonds"}],
+                    [{10,"Diamonds"},{"Q","Clubs"}]) =:= equal),
+   ?_assert(compare([{7,"Clubs"},{"Q","Diamonds"}],
+                    [{10,"Diamonds"},{"A","Clubs"}]) =:= smaller)
+  ].
--- hai/ch8/game.erl
+++ hai/ch8/game.erl
@@ -120,36 +120,3 @@ player(Cards) ->
       player(Cards ++ GivenCards)
   end.
 
-compare(Piles1, Piles2) ->
-  io:format("Now compare ~p with ~p ~n", [Piles1, Piles2]),
-  LastOfPiles1 = lists:last(Piles1),
-  LastOfPiles2 = lists:last(Piles2),
-  {Value1, _} = LastOfPiles1,
-  {Value2, _} = LastOfPiles2,
-  if Value1 == Value2 ->
-      io:format("~p equals to ~p ~n", [Piles1, Piles2]),
-      equal;
-    Value1 =:= "A" ->
-      io:format("~p is bigger than  ~p ~n", [Piles1, Piles2]),
-      bigger;
-    Value2 =:= "A" ->
-      io:format("~p is smaller than  ~p ~n", [Piles1, Piles2]),
-      smaller;
-    Value1 > Value2 ->
-      io:format("~p is bigger than  ~p ~n", [Piles1, Piles2]),
-      bigger;
-    Value1 < Value2 ->
-      io:format("~p is smaller than  ~p ~n", [Piles1, Piles2]),
-      smaller
-  end.
-
-compare_test_() ->
-  [?_assert(compare([{7,"Clubs"},{9,"Diamonds"}],
-                    [{10,"Diamonds"},{10,"Clubs"}]) =:= smaller),
-   ?_assert(compare([{7,"Clubs"},{"Q","Diamonds"}],
-                    [{10,"Diamonds"},{10,"Clubs"}]) =:= bigger),
-   ?_assert(compare([{7,"Clubs"},{"Q","Diamonds"}],
-                    [{10,"Diamonds"},{"Q","Clubs"}]) =:= equal),
-   ?_assert(compare([{7,"Clubs"},{"Q","Diamonds"}],
-                    [{10,"Diamonds"},{"A","Clubs"}]) =:= smaller)
-  ].
