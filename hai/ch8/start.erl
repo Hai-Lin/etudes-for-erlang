7a523492cf349119471a8f64642496aed89882b6
Hai Lin <hai.lin.cs@gmail.com>
2013-07-07 21:04:21 -0400

    Add guard to start_game

--- hai/ch8/start.erl
+++ hai/ch8/start.erl
@@ -2,5 +2,8 @@
 -module(start).
 -export([start_game/1]).
 
-start_game(NumberOfCards) ->
-  spawn(game, dealer, [{init, NumberOfCards}]).
+start_game(NumberOfCards) when NumberOfCards =< 26 ->
+  spawn(game, dealer, [{init, NumberOfCards}]);
+
+start_game(_) ->
+  io:format("Start wish any number between 1-26 ~n").
