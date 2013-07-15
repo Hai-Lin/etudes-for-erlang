15283b8fa728f7c67b33d48e7e3659cbe344386b
Hai Lin <hai.lin.cs@gmail.com>
2013-07-14 23:01:18 -0400

    Add read file

--- hai/ch10/phone_ets.erl
+++ hai/ch10/phone_ets.erl
@@ -3,9 +3,21 @@
 -export([setup/1]).
 
 setup(File) ->
-  try Result = file:open(File, [read])
+  try setup_ets_table(file:open(File, [read]))
   catch
     error:Error -> {error, Error}
   end.
+
+setup_ets_table({_, InputFile}) ->
+  Line = io:get_line(InputFile, ""),
+  if Line /= eof ->
+      write_ets(Line),
+      setup_ets_table({ok, InputFile});
+    true ->
+      io:format("EOF, exitting..~n")
+  end.
+
+write_ets(Line) ->
+  io:format(Line).
 
 
