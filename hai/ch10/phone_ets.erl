7bb1d7a569e3eb7879de434d0d60e3c48c47e04f
Hai Lin <hai.lin.cs@gmail.com>
2013-07-16 17:40:17 -0400

    Add calculate time

--- hai/ch10/phone_ets.erl
+++ hai/ch10/phone_ets.erl
@@ -1,14 +1,11 @@
 
 -module(phone_ets).
--export([setup/1]).
+-export([setup/1, calculate_time_list/0]).
 -include("phone_records.hrl").
 
 setup(File) ->
   init_table(),
   setup_ets_table(file:open(File, [read])).
-  %catch
-    %error:Error -> {error, Error}
-  %end.
 
 init_table() ->
   case ets:info(phone_calls) of
@@ -23,12 +20,34 @@ setup_ets_table({_, File}) ->
       write_ets(Line),
       setup_ets_table({ok, File});
     true ->
-      io:format("EOF, exitting..~n"),
+      io:format("Done importing table~n"),
       ets:info(phone_calls)
   end.
 
 write_ets(Line) ->
-  [PhoneNumber, StartingDate, StartingTime, EndDate, EndTime] = re:split(Line,",",[{return,list}]),
-  ets:insert(phone_calls, #phone_call{phone_number=PhoneNumber, start_date=StartingDate, start_time=StartingTime, end_date=EndDate, end_time=EndTime}).
+  [PhoneNumber, StartDate, StartTime, EndDate, EndTime] = re:split(re:replace(Line, "\\s+", "", [global]),",",[{return,list}]),
+  ets:insert(phone_calls, #phone_call{phone_number=PhoneNumber, start_date=StartDate, start_time=StartTime, end_date=EndDate, end_time=EndTime}).
 
+calculate_time_list() ->
+  [{PhoneNumber, calculate_time({StartDate, StartTime}, {EndDate, EndTime})} ||  {_, PhoneNumber, StartDate, StartTime, EndDate, EndTime} <- ets:tab2list(phone_calls)].
 
+calculate_time(StartTime, EndTime) ->
+  calculate_seconds(EndTime) - calculate_seconds(StartTime).
+
+calculate_seconds({Date, Time}) ->
+  calendar:datetime_to_gregorian_seconds({parse_date(Date), parse_time(Time)}).
+
+parse_time(Time) ->
+  [Hours, Minutes, Seconds] = re:split(Time, ":", [{return,list}]),
+  {string_to_number(Hours), string_to_number(Minutes), string_to_number(Seconds)}.
+
+parse_date(Date) ->
+  [Year, Month, Day] = re:split(Date, "-", [{return,list}]),
+  {string_to_number(Year), string_to_number(Month), string_to_number(Day)}.
+  
+string_to_number(String) ->
+  Number = string:to_float(String),
+  case Number of
+    {error,_} -> element(1, string:to_integer(String));
+    _ -> element(1, Number)
+  end.
