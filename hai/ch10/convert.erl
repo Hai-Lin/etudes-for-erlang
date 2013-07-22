0ff01e1eff4080d6a6c2846637609c85e588b19a
Hai Lin <hai.lin.cs@gmail.com>
2013-07-22 11:42:14 -0400

    Move convert function to convert module

---
+++ hai/ch10/convert.erl
@@ -0,0 +1,27 @@
+-module(convert).
+-export([calculate_time/2, string_to_time/1, string_to_date/1]).
+
+
+calculate_time(StartTime, EndTime) ->
+  seconds_to_minutes(calculate_seconds(EndTime) - calculate_seconds(StartTime)).
+
+seconds_to_minutes(Seconds) ->
+  (Seconds + 59) div 60.
+
+calculate_seconds({Date, Time}) ->
+  calendar:datetime_to_gregorian_seconds({string_to_date(Date), string_to_time(Time)}).
+
+string_to_time(Time) ->
+  [Hours, Minutes, Seconds] = re:split(Time, ":", [{return,list}]),
+  {string_to_number(Hours), string_to_number(Minutes), string_to_number(Seconds)}.
+
+string_to_date(Date) ->
+  [Year, Month, Day] = re:split(Date, "-", [{return,list}]),
+  {string_to_number(Year), string_to_number(Month), string_to_number(Day)}.
+
+string_to_number(String) ->
+  Number = string:to_float(String),
+  case Number of
+    {error,_} -> element(1, string:to_integer(String));
+    _ -> element(1, Number)
+  end.
--- hai/ch10/phone_ets.erl
+++ hai/ch10/phone_ets.erl
@@ -64,28 +64,5 @@ write_phone_calls_table(Line) ->
   ets:insert(phone_calls, #phone_call{phone_number=PhoneNumber, start_date=StartDate, start_time=StartTime, end_date=EndDate, end_time=EndTime}).
 
 calculate_time_list() ->
-  [{PhoneNumber, calculate_time({StartDate, StartTime}, {EndDate, EndTime})} ||  {_, PhoneNumber, StartDate, StartTime, EndDate, EndTime} <- ets:tab2list(phone_calls)].
+  [{PhoneNumber, convert:calculate_time({StartDate, StartTime}, {EndDate, EndTime})} ||  {_, PhoneNumber, StartDate, StartTime, EndDate, EndTime} <- ets:tab2list(phone_calls)].
 
-calculate_time(StartTime, EndTime) ->
-  seconds_to_minutes(calculate_seconds(EndTime) - calculate_seconds(StartTime)).
-
-seconds_to_minutes(Seconds) ->
-  (Seconds + 59) div 60.
-
-calculate_seconds({Date, Time}) ->
-  calendar:datetime_to_gregorian_seconds({parse_date(Date), parse_time(Time)}).
-
-parse_time(Time) ->
-  [Hours, Minutes, Seconds] = re:split(Time, ":", [{return,list}]),
-  {string_to_number(Hours), string_to_number(Minutes), string_to_number(Seconds)}.
-
-parse_date(Date) ->
-  [Year, Month, Day] = re:split(Date, "-", [{return,list}]),
-  {string_to_number(Year), string_to_number(Month), string_to_number(Day)}.
-
-string_to_number(String) ->
-  Number = string:to_float(String),
-  case Number of
-    {error,_} -> element(1, string:to_integer(String));
-    _ -> element(1, Number)
-  end.
