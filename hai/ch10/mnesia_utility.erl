9362d1b38253a223f3013af1cb093c34b23a11c2
Hai Lin <hai.lin.cs@gmail.com>
2013-07-22 11:28:28 -0400

    Add read file to mnesia_utility

--- hai/ch10/mnesia_utility.erl
+++ hai/ch10/mnesia_utility.erl
@@ -1,14 +1,22 @@
 -module(mnesia_utility).
 -export([fill_table/5]).
 
-
 fill_table(TableName, FileName, AddDataFun, RecordInfo, TableType) ->
   init_table(TableName, RecordInfo, TableType),
-  mnesia:transaction(AddDataFun(FileName)).
-
+  mnesia:transaction(read_file(AddDataFun,file:open(FileName, [read]))).
 
 init_table(TableName, RecordInfo, TableType) ->
   mnesia:create_schema([node()]),
   mnesia:start(),
   mnesia:create_table(TableName,[{type, TableType}, {attributes, RecordInfo}]).
 
+read_file(AddDataFun, File) ->
+  Content = io:get_line(File, ""),
+  case Content of 
+    eof ->
+      false;
+    _ ->
+      Items = re:split(re:replace(Content, "\\s+", "", [global]),",",[{return,list}]),
+      AddDataFun(Items),
+      read_file(AddDataFun, File)
+  end.
