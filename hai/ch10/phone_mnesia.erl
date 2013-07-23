5ec1d6f4a5f21c3cd557c533db8c8c73427672b4
Hai Lin <hai.lin.cs@gmail.com>
2013-07-23 15:45:28 -0400

    Show sum of minutes and rate

--- hai/ch10/phone_mnesia.erl
+++ hai/ch10/phone_mnesia.erl
@@ -2,24 +2,33 @@
 -export([setup/0, summary/3]).
 -include("phone_records.hrl").
 -include_lib("stdlib/include/qlc.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 setup() ->
   mnesia_setup:setup().
 
 summary(LastName, FirstName, MiddleName) ->
-  mnesia:transaction(
-    fun() -> qlc:e(
-          qlc:q([{C#customer.phone_number, convert:calculate_number_time({P#phone_call.start_date, P#phone_call.start_time}, {P#phone_call.end_date, P#phone_call.end_time}),
-                  (convert:calculate_number_time({P#phone_call.start_date, P#phone_call.start_time}, {P#phone_call.end_date, P#phone_call.end_time}))*C#customer.rate} || 
-              P <- mnesia:table(phone_call),
-              C <- mnesia:table(customer),
-              C#customer.first_name == FirstName,
-              C#customer.last_name == LastName,
-              C#customer.middle_name == MiddleName,
-              P#phone_call.phone_number == C#customer.phone_number])
-          )
-    end
-    ).
+  {atomic, Results} = mnesia:transaction(
+      fun() -> qlc:e(
+            qlc:q([{C#customer.phone_number, C#customer.rate, convert:calculate_number_time({P#phone_call.start_date, P#phone_call.start_time}, {P#phone_call.end_date, P#phone_call.end_time})} ||
+                P <- mnesia:table(phone_call),
+                C <- mnesia:table(customer),
+                C#customer.first_name == FirstName,
+                C#customer.last_name == LastName,
+                C#customer.middle_name == MiddleName,
+                P#phone_call.phone_number == C#customer.phone_number])
+            )
+      end
+      ),
+  Minutes = lists:foldl(fun total_minutes/2, 0, Results),
+  {Number, Rate, _} = hd(Results),
+  {Number, Minutes, Minutes * Rate}.
+
+total_minutes({_, _, Minutes}, Accumlator) -> 
+  Accumlator + Minutes.
 
 
-
+summary_test_() ->
+  setup(),
+  [?_assert(summary("Smith", "Samuel", "Steven") =:= {"838-555-1099",9,0.9}),
+   ?_assert(summary("Nakamura", "Noriko", "") =:= {"213-555-0172",9,1.08})].
