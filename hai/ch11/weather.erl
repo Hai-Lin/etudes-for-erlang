b1df95214ed224c12dd01818c0e7a1c1116da453
Hai Lin <hai.lin.cs@gmail.com>
2013-07-30 17:07:20 -0400

    Get weather.erl working

--- hai/ch11/weather.erl
+++ hai/ch11/weather.erl
@@ -15,7 +15,8 @@ init([]) ->
   {ok, []}.
 
 handle_call(_Request, _From, State) ->
-  {reply, {ok, handle_request(_Request)}, State ++ _Request}.
+  {Result,Data} = handle_request(_Request),
+  {reply, {Result, Data}, State ++ _Request}.
 
 handle_cast(_Message, State) ->
   io:format("Most recent requests: ~p~n", [State]),
@@ -32,5 +33,49 @@ code_change(_OldVersion, State, _Extra) ->
 
 handle_request(StationCode) ->
   URL = "http://w1.weather.gov/xml/current_obs/" ++ StationCode ++ ".xml",
-  {Result, Info} = httpc:request(URL).
+  {Result, Info} = httpc:request(URL),
+  case Result of 
+    error -> {error, Info};
+    ok ->
+      handle_success_request(Info)
+  end.
 
+handle_success_request({{_, Code, _}, _, Data}) ->
+  case Code of 
+    200 ->
+      {ok, analyze_info(Data)};
+    _ ->
+      {error, Code}
+  end.
+
+%% Take raw XML data and return a set of {key, value} tuples
+
+analyze_info(WebData) ->
+  %% list of fields that you want to extract
+  ToFind = [location, observation_time_rfc822, weather, temperature_string],
+
+  %% get just the parsed data from the XML parse result
+  Parsed = element(1, xmerl_scan:string(WebData)),
+
+  %% This is the list of all children under <current_observation>
+  Children = Parsed#xmlElement.content,
+
+  %% Find only XML elements and extract their names and their text content.
+  %% You need the guard so that you don't process the newlines in the
+  %% data (they are XML text descendants of the root element).
+  ElementList = [{El#xmlElement.name, extract_text(El#xmlElement.content)}
+    || El <- Children, element(1, El) == xmlElement],
+
+  %% ElementList is now a keymap; get the data you want from it.
+  lists:map(fun(Item) -> lists:keyfind(Item, 1, ElementList) end, ToFind).
+
+
+%% Given the parsed content of an XML element, return its first node value
+%% (if it's a text node); otherwise return the empty string.
+
+extract_text(Content) ->
+  Item = hd(Content),
+  case element(1, Item) of
+    xmlText -> Item#xmlText.value;
+    _ -> ""
+  end.
