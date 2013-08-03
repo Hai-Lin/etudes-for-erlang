ce8edd43dc739e56b57074d3bd8f040d09268a0b
Hai Lin <hai.lin.cs@gmail.com>
2013-08-03 09:29:28 -0400

    Finish person module

--- hai/ch11/person.erl
+++ hai/ch11/person.erl
@@ -1,10 +1,22 @@
 -module(person).
 -export([start_link/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--define(SERVER, ?MODULE).
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3,
+         get_chat_node/0,
+         login/1,
+         logout/0,
+         say/1,
+         users/0,
+         who/2,
+         set_profile/2]).
+-define(CLIENT, ?MODULE).
 
 start_link(Server) ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, Server, []).
+  gen_server:start_link({local, ?CLIENT}, ?MODULE, Server, []).
 
 init(Server) ->
   {ok, [Server, []]}.
@@ -29,8 +41,12 @@ handle_call(get_profile, _From, State) ->
   {_, Profile} = State,
   {reply, {ok, Profile}, State};
 
+handle_call(get_user, _From, State) ->
+  {_, {User, _}} = State,
+  {reply, {ok, User}, State};
+
 handle_call({set_profile, Key, Value}, _From, State) ->
-  {Node, Profile} = State, 
+  {Node, {_, Profile}} = State,
   NewProfile = set_profile(Key, Value, Profile),
   {reply, {ok, "Set " ++ Key ++ " to " ++ Value}, {Node, NewProfile}}.
 
@@ -42,3 +58,38 @@ set_profile(Key, Value, Profile) ->
       lists:keyreplace(Key, 1, Profile, {Key, Value})
   end.
 
+get_chat_node() ->
+  gen_server:call(person, get_chat_node).
+
+login(UserName) ->
+  UserNameAfterParse = parse_user_name(UserName),
+  case UserNameAfterParse of 
+    {ok, StringUserName} ->
+      gen_server:call(chatroom, {login, StringUserName});
+    {error, Error} ->
+      io:format("~p~n", Error)
+  end.
+
+parse_user_name(UserName)  when is_atom(UserName) ->
+  {ok, atom_to_list(UserName)};
+
+parse_user_name(UserName)  when is_list(UserName) ->
+  {ok, UserName};
+
+parse_user_name(_) ->
+  {error, "User name should be a string or atom."}.
+
+logout() ->
+  gen_server:call(chatroom, logout).
+
+say(Text) ->
+  gen_server:call(chatroom, {say, Text}).
+
+users() ->
+  gen_server:call(chatroom, users).
+
+who(UserName, UserNode) ->
+  gen_server:call(chatroom, {who, UserName, UserNode}).
+
+set_profile(Key, Value) ->
+  gen_server:call(?CLIENT, {set_profile, Key, Value}).
